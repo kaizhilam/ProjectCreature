edd5b6d5b9571910f67a4240376f40fc5ab311a2
bradulz <bradjamesgrant@gmail.com>
2019-04-05 17:38:32 +1100

    same as prev

---
+++ Assets/Scripts/Ability/JumpAbility.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpAbility : PassiveAbility
+{
+    GameObject Player;
+    Rigidbody Rigid;
+    public JumpAbility()
+    {
+        Player = GameObject.Find("Player");
+        Rigid = (Rigidbody)Player.GetComponent(typeof(Rigidbody));
+    }
+    
+    public override void Equipped()
+    {
+        Rigid.mass *= 2;
+    }
+    public override void Unequipped()
+    {
+        Rigid.mass /= 2;
+    }
+}
