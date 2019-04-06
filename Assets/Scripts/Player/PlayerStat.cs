11af50ff83e7ac34854702c72d2a1e4a4651b0a1
unknown <myli981014@gmail.com>
2019-04-06 17:21:27 +1100

    Added project file

--- Assets/Scripts/Player/PlayerStat.cs
+++ Assets/Scripts/Player/PlayerStat.cs
@@ -6,5 +6,14 @@ public class PlayerStat : MonoBehaviour
 {
     public static float HP = 100f;
     //public ProjectileScriptableObject ProjectileAbility;
-    public static PlayerInventory<string> Inventory = new PlayerInventory<string>(); //change int to whatever type item is 
+    //public static PlayerInventory<string> Inventory = new PlayerInventory<string>(); //change int to whatever type item is 
+    //public static PlayerInventory<Item> Inventory = new PlayerInventory<Item>();
+    //public PlayerInventory<Item> Inventory = new PlayerInventory<Item>();
+    /*
+    private void Start()
+    {
+        Inventory.Capacity = 9999;
+    }
+    */
+
 }
