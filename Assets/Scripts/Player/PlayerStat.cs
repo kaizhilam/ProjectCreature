cf8d60333b7831fb06c14fcd71073eb39810291a
kaizhilam <kaizhilam@gmail.com>
2019-04-08 23:52:22 +1000

    Added blink ability, remove looking up clamp on camera (for blink), changed text UI to display which active ability is current;y equipped

--- Assets/Scripts/Player/PlayerStat.cs
+++ Assets/Scripts/Player/PlayerStat.cs
@@ -11,10 +11,11 @@ public class PlayerStat : MonoBehaviour
 	 * LightningballAbility
 	 * FastMovementAbility
 	 * HighJumpAbility
+	 * BlinkAbility
 	 * */
     public static float HP = 100f;
     public static PlayerInventory<string> Inventory = new PlayerInventory<string>(); //change int to whatever type item is 
-	public static Ability[] Abilities = new Ability[3] { new FireballAbility(), new WaterballAbility(), new LightningballAbility() }; //change ability here
+	public static Ability[] Abilities = new Ability[3] { new BlinkAbility(), new WaterballAbility(), new LightningballAbility() }; //change ability here
 	public static int AbilitiesIndex = 0;
 	public static Ability SelectedAbility = Abilities[AbilitiesIndex];
 	private void Update()
