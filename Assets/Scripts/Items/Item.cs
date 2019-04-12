533f987563cfb94574850e16949926d5869d783d
李茂焱 <myl@limaoyandeMacBook-Pro.local>
2019-04-12 20:34:24 +1000

    Item child class and base json(ItemManagement)

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -1,71 +1,3 @@
-<<<<<<< HEAD
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Item : MonoBehaviour
-{
-    public int objID;
-    public string objName;
-    public GameObject player;
-    private float distance;
-    private bool isCloseEnough;
-    
-
-    private void Update()
-    {
-        judgeDistance();
-    }
-
-    private void judgeDistance()
-    {
-        distance = Vector3.Distance(transform.position, player.transform.position);
-        if (distance < 5)
-        {
-            GetComponent<MeshRenderer>().material.color = Color.red;
-            isCloseEnough = true;
-        }
-        else
-        {
-            GetComponent<MeshRenderer>().material.color = Color.white;
-            isCloseEnough = false;            
-        }
-    }
-
-    public bool IsCloseEnough()
-    {
-        return isCloseEnough;
-    }
-
-    
-
-    //public ItemScriptableObject _Base;
-
-    //public string Name { get; private set; }
-    //public string Description { get; private set; }
-    //public Mesh Mesh { get; private set; }
-    //public Material Material { get; private set; }
-
-    //private void Start()
-    //{
-    //    Name = _Base.Name;
-    //    Description = _Base.Description;
-    //    Mesh = _Base.Mesh;
-    //    Material = _Base.Material;
-
-    //    MeshCollider mesh = gameObject.AddComponent<MeshCollider>();
-    //    mesh.sharedMesh = Mesh;
-    //    mesh.convex = true;
-    //    gameObject.AddComponent<MeshRenderer>();
-    //    gameObject.AddComponent<Rigidbody>();
-    //}
-
-    //private void Update()
-    //{
-    //    Graphics.DrawMesh(Mesh, transform.position, transform.rotation, Material, 0);
-    //}
-}
-=======
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -152,4 +84,3 @@ public class Item : MonoBehaviour
     //    Graphics.DrawMesh(Mesh, transform.position, transform.rotation, Material, 0);
     //}
 }
->>>>>>> master
--- Assets/Scripts/Player/PlayerStat.cs
+++ Assets/Scripts/Player/PlayerStat.cs
@@ -14,19 +14,6 @@ public class PlayerStat : MonoBehaviour
 	 * BlinkAbility
 	 * */
     public static float HP = 100f;
-<<<<<<< HEAD
-    //public ProjectileScriptableObject ProjectileAbility;
-    //public static PlayerInventory<string> Inventory = new PlayerInventory<string>(); //change int to whatever type item is 
-    //public static PlayerInventory<Item> Inventory = new PlayerInventory<Item>();
-    //public PlayerInventory<Item> Inventory = new PlayerInventory<Item>();
-    /*
-    private void Start()
-    {
-        Inventory.Capacity = 9999;
-    }
-    */
-
-=======
     public static PlayerInventory<string> Inventory = new PlayerInventory<string>(); //change int to whatever type item is 
 	public static Ability[] Abilities = new Ability[3] { new BlinkAbility(), new WaterballAbility(), new LightningballAbility() }; //change ability here
 	public static int AbilitiesIndex = 0;
@@ -35,5 +22,4 @@ public class PlayerStat : MonoBehaviour
 	{
 		SelectedAbility = Abilities[AbilitiesIndex];
 	}
->>>>>>> master
 }
