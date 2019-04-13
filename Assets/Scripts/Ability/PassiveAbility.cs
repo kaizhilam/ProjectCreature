ca45da2a266d2fd5ed16d052c82e6e4f2a3e17f3
bradulz <bradjamesgrant@gmail.com>
2019-04-13 12:37:48 +1000

    deleting ablity code

--- Assets/Scripts/Ability/PassiveAbility.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class PassiveAbility : Ability
-{
-    public abstract void Equipped();
-    public abstract void Unequipped();
-}
