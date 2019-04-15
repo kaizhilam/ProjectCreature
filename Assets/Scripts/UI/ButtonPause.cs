0e0b1e4bae905aaaaea99bed671dc10bd3ff7ecf
galgame-sh <35554042+galgame-sh@users.noreply.github.com>
2019-04-16 03:15:51 +1000

    Pause

---
+++ Assets/Scripts/UI/ButtonPause.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPause : MonoBehaviour
+{
+
+    //the ButtonPauseMenu
+    public GameObject ingameMenu;
+    bool paused;
+    void Update()
+    {
+        if (Input.GetButtonUp("Cancel"))
+        {
+            if (!paused)
+            {
+                OnPause();
+            }
+            else
+                OnResume();
+        }
+    }
+        public void OnPause()
+    {
+        Time.timeScale = 0;
+        ingameMenu.SetActive(true);
+        paused = true;
+    }
+
+    public void OnResume()
+    {
+        Time.timeScale = 1f;
+        ingameMenu.SetActive(false);
+        paused = false;
+    }
+    
+    }
