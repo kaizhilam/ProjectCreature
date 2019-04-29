1e63f21eb6131b59f5589ff26a198a5e8d36c9c3
KAI LAM <kzlam@deakin.edu.au>
2019-04-29 16:15:47 +1000

    Changed spelling error because BRAD MADE ME DO IT, HE IS AN EVIL PERSON

--- Assets/Scripts/GameController/Quest.cs
+++ Assets/Scripts/GameController/Quest.cs
@@ -7,6 +7,6 @@ public class Quest
     public string Name;
     public string Description;
     public List<Quest> Prerequisite;
-    public bool isCompleted;
-    public bool isprerequisite;
+    public bool IsCompleted;
+    public bool IsPrerequisite;
 }
