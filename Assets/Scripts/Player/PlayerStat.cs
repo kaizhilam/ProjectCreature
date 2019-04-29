4a9e672b0933b5ce7e776c1762626012c4aaec6c
DavidZhang23 <41571638+DavidZhang23@users.noreply.github.com>
2019-04-29 22:47:46 +1000

    D:/Unity Project/ProjectCreature/Assets/Scripts/Enemy/EnemyAI.cs

--- Assets/Scripts/Player/PlayerStat.cs
+++ Assets/Scripts/Player/PlayerStat.cs
@@ -14,7 +14,7 @@ public class PlayerStat : MonoBehaviour
 	 * */
     public static float HP = 100f;
     public static PlayerInventory<string> Inventory = new PlayerInventory<string>(); //change int to whatever type item is 
-	public static Ability[] Abilities = new Ability[3] { new FireballAbility(), new WaterballAbility(), new LightningballAbility()}; //change ability here
+	public static Ability[] Abilities = new Ability[4] { new FireballAbility(), new WaterballAbility(), new LightningballAbility(), new InvisibleAbility()}; //change ability here
 	public static int AbilitiesIndex = 0;
 	public static Ability SelectedAbility = Abilities[AbilitiesIndex];
 	private void Update()
