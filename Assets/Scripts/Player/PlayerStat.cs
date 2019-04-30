5860b34998a295edbbda47039c29bd74b517beb7
bradulz <bradjamesgrant@gmail.com>
2019-04-30 22:37:24 +1000

    removing ability system, starting on weapon system

--- Assets/Scripts/Player/PlayerStat.cs
+++ Assets/Scripts/Player/PlayerStat.cs
@@ -15,11 +15,4 @@ public class PlayerStat : MonoBehaviour
 	 * */
     public static float HP = 100f;
     public static PlayerInventory<string> Inventory = new PlayerInventory<string>(); //change int to whatever type item is 
-	public static Ability[] Abilities = new Ability[3] { new BlinkAbility(), new WaterballAbility(), new LightningballAbility() }; //change ability here
-	public static int AbilitiesIndex = 0;
-	public static Ability SelectedAbility = Abilities[AbilitiesIndex];
-	private void Update()
-	{
-		SelectedAbility = Abilities[AbilitiesIndex];
-	}
 }
