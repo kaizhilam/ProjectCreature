5332ece94e6c49c6a869eab4dd9386dd77e4074b
bradulz <bradjamesgrant@gmail.com>
2019-05-03 16:48:31 +1000

    making a start to state machines

---
+++ Assets/Scripts/States/StateMachine.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class StateMachine : MonoBehaviour
+{
+    private Dictionary<Type, State> _availableStates;
+
+    public State CurrentState { get; private set; }
+    public event Action<State> OnStateChanged;
+
+    public void SetStates(Dictionary<Type, State> states)
+    {
+        _availableStates = states;
+    }
+
+    private void Update()
+    {
+        if(Current)
+    }
+}
