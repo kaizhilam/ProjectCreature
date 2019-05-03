05510b6f20b3be5668e9d44ab69cc7f61ef6e029
bradulz <bradjamesgrant@gmail.com>
2019-05-03 23:10:13 +1000

    adding knife in, inv now scales with screen size

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager instance = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+}
