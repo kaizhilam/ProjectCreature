4c9992c4f20125759ddaf7f9a588115fcaa14230
bradulz <bradjamesgrant@gmail.com>
2019-05-05 20:40:45 +1000

    reverting movement code, removed delegate event for movement, saving player prefab to include InputManager

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 public class InputManager : MonoBehaviour
 {
     public delegate void InputDelegate();
-    public delegate void MovementDelegate(Vector3 input, Vector3 raw);
     public event InputDelegate LeftClick;
     public event InputDelegate RightClick;
     public event InputDelegate BKey;
     public event InputDelegate EKey;
-    //public event InputDelegate ShiftKey; not yet
-    public event MovementDelegate WASDJump;
+    //public event InputDelegate ShiftKey; soon
     private Vector3 _Input, _InputRaw;
 
     public static InputManager instance = null;
@@ -32,37 +30,16 @@ public class InputManager : MonoBehaviour
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.B)) {
-            if (BKey != null)
-            {
-                BKey();
-            }
+            BKey?.Invoke();
         }
         if (Input.GetMouseButtonDown(0)) {
-            if (LeftClick != null)
-            {
-                LeftClick();
-            }
+            LeftClick?.Invoke();
         }
         if (Input.GetMouseButtonDown(1)) {
-            if (RightClick != null)
-            {
-                RightClick();
-            }
+            RightClick?.Invoke();
         }
         if (Input.GetKeyDown(KeyCode.E)) {
-            if (EKey != null)
-            {
-                EKey();
-            }
-        }
-        _Input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Jump"), Input.GetAxis("Vertical")); //NUMBER CONTAINS DECIMALS FOR CHARACTER ACCELERATION
-        _InputRaw = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Jump"), Input.GetAxisRaw("Vertical")); //NUMBER IS A FULL NUMBER FOR CHECKING IF BUTTON IS PRESSED
-        if (_InputRaw!= Vector3.zero)
-        {
-            if (WASDJump != null)
-            {
-                WASDJump(_Input, _InputRaw);
-            }
+            EKey?.Invoke();
         }
 
     }
