a6432cbf7662af44a00a25accd33cf9c916f0e6b
kaizhilam <kaizhilam@gmail.com>
2019-05-13 01:37:09 +1000

    Working on arrow projectile

---
+++ Assets/Scripts/Weapons/Arrow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+	//private Rigidbody _Rigidbody;
+
+	private void Start()
+    {
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		GameObject camera = GameObject.FindGameObjectWithTag("MainCamera");
+		//transform.SetPositionAndRotation(player.transform.position + (Vector3.forward * 5), player.transform.rotation);
+	}
+
+	private void Update()
+    {
+    }
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log(collision.gameObject.name);
+		//Destroy(_Rigidbody);
+	}
+}
