435bad28b1a3975aec736770e607f199901e7c55
KAI LAM <kzlam@deakin.edu.au>
2019-05-13 14:26:08 +1000

    Fixed arrow starting location and direction

--- Assets/Scripts/Weapons/Arrow.cs
+++ Assets/Scripts/Weapons/Arrow.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
-	//private Rigidbody _Rigidbody;
+	private Rigidbody _Rigidbody;
+    public float Speed;
 
 	private void Start()
     {
+        _Rigidbody = GetComponent<Rigidbody>();
 		GameObject player = GameObject.FindGameObjectWithTag("Player");
 		GameObject camera = GameObject.FindGameObjectWithTag("MainCamera");
-		//transform.SetPositionAndRotation(player.transform.position + (Vector3.forward * 5), player.transform.rotation);
-	}
-
-	private void Update()
-    {
+        transform.SetPositionAndRotation(player.transform.position + (Vector3.forward*2), player.transform.rotation);
+        transform.LookAt(ThirdPersonCamera.LookingAtPoint);
     }
 
-	private void OnCollisionEnter(Collision collision)
+    private void FixedUpdate()
+    {
+        _Rigidbody.AddForce(Vector3.forward * Speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
 	{
 		Debug.Log(collision.gameObject.name);
 		//Destroy(_Rigidbody);
