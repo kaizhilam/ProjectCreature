b66e097dde5ec8355f40d6d9ed5558d07c2baa94
unknown <1095940672@qq.com>
2019-05-14 14:39:56 +1000

    add cooldown mask on hotbar and hp status bar

---
+++ Assets/Hp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hp : MonoBehaviour
+{
+    public float MaxHp;
+    public float hp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.localPosition = new Vector3((-200+200*(hp/MaxHp)),0.0f,0.0f);
+    }
+}
