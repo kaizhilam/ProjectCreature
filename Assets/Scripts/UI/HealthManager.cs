e72faec18a7806daeabc648d4a043bdfddd6e08a
kaizhilam <kaizhilam@gmail.com>
2019-09-04 13:53:42 +1000

    Implemented David's health bar

--- Assets/Scripts/UI/HealthManager.cs
+++ Assets/Scripts/UI/HealthManager.cs
@@ -7,10 +7,12 @@ public class HealthManager : MonoBehaviour
 {
 	private float _MaxHealth;
 	private float _Health;
+	private Vector2 _HealthBarSize;
 
 	private void Start()
 	{
 		_MaxHealth = Player.HP;
+		_HealthBarSize = transform.GetChild(2).GetComponent<RectTransform>().sizeDelta;
 	}
 
 	private void Update()
@@ -22,14 +24,14 @@ public class HealthManager : MonoBehaviour
 	private void UpdateHealthBar()
 	{
 		_Health = Player.HP;
-		Transform healthBar = transform.GetChild(0); //REFERENCE HEATHBAR
-		healthBar.GetComponent<RectTransform>().sizeDelta = new Vector2(20.48f * _Health, 534); //SET HEALTH BAR WIDTH
+		Transform healthBar = transform.GetChild(2); //REFERENCE HEATHBAR
+		healthBar.GetComponent<RectTransform>().sizeDelta = new Vector2(_HealthBarSize.x *_Health/_MaxHealth, 160); //SET HEALTH BAR WIDTH
 		//Debug.Log(_Health);
 	}
 
 	private void UpdateHealthText()
 	{
-		Transform heathText = transform.GetChild(2);
+		Transform heathText = transform.GetChild(1);
 		heathText.GetComponent<Text>().text = Mathf.Round(_Health) + "/" + _MaxHealth;
 	}
 }
