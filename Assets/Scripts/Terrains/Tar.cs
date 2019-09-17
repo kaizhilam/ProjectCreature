3d875dc5b4ae98bb619049580e7b153892dd1d1c
kaizhilam <kaizhilam@gmail.com>
2019-09-17 21:57:48 +1000

    Added tar script

---
+++ Assets/Scripts/Terrains/Tar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tar : MonoBehaviour
+{
+	private float _TempPlayerMovement;
+	public float SlowDownFactor;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			_TempPlayerMovement = other.gameObject.GetComponent<PlayerMovement>().Speed;
+			other.gameObject.GetComponent<PlayerMovement>().Speed = _TempPlayerMovement / SlowDownFactor;
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			other.gameObject.GetComponent<PlayerMovement>().Speed = _TempPlayerMovement;
+		}
+	}
+}
