ea8f2d183d4cc818e0dd247e18325da0da90c6a4
unknown <1095940672@qq.com>
2019-09-19 18:19:03 +1000

    ability UI

---
+++ Assets/Scripts/Abilities/AbilityController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityController : MonoBehaviour
+{
+    private GameObject _player;
+    public Image DashImage;
+    public Image DJumpImage;
+    public Image ClimbImage;
+    public Image GrappleImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.Find("Player");
+        DashImage.fillAmount = 0;
+        DJumpImage.fillAmount = 0;
+        ClimbImage.fillAmount = 0;
+        GrappleImage.fillAmount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider abilityCrystal)
+    {
+        if (abilityCrystal.gameObject.CompareTag("Dash"))
+        {
+            abilityCrystal.gameObject.SetActive(false);
+            DashImage.fillAmount = 1;
+            _player.AddComponent<AbilityDash>();
+        }
+        else if (abilityCrystal.gameObject.CompareTag("DoubleJump"))
+        {
+            abilityCrystal.gameObject.SetActive(false);
+            DJumpImage.fillAmount = 1;
+            _player.AddComponent<AbilityDoubleJump>();
+        }
+        else if (abilityCrystal.gameObject.CompareTag("WallClimb"))
+        {
+            abilityCrystal.gameObject.SetActive(false);
+            ClimbImage.fillAmount = 1;
+            _player.AddComponent<AbilityWallClimb>();
+        }
+        else if (abilityCrystal.gameObject.CompareTag("Grapple"))
+        {
+            abilityCrystal.gameObject.SetActive(false);
+            GrappleImage.fillAmount = 1;
+            _player.AddComponent<AbilityGrapple>();
+        }
+    }
+}
